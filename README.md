ab9ae4a7697039520e1c4e9ebb129efe6419ca98
David Hunter <dph@alumni.neu.edu>
2019-06-16 14:21:29 -0700

    First working draft of disk_check.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # freenasutils
 Misc utilities used for managing and monitoring FreeNAS
+
+* disk_check.sh - Looks at all disks on the system and sends a quick SMART status to an email address
+* avscan.sh - Updates ClamAV definitions, runs a scan and then sends an email on the results.
+* run_clamav_scan.sh - Used to run the avscan.sh script from cron.
--- disk_check.sh
+++ disk_check.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
+#
 # Description:
 #   A simple script to send a formatted email regarding the status of disks based upon smartctl.
 #   Place this in  location for administrative scripts and then setup a cron job to execute.
 # Usage:
 #   sh disk_check.sh
 # ToDo:
-#   Make things more readable and use more variables for output file and disks.
+#   Make the DEBUG a procedure
+#
 
-# generate a list of disks on the host
+O_FILE=disk_overview.html
+
+# Generate a list of disks on the host and ones to exclude
+# NB:  We could use smartctl --scan but that will not list all the disks on the sytem.
 LIST_OF_DISKS=$(geom disk list | grep Name | awk '{print $3}')
-
-# TBD: specify any disks to exclude
 EXCLUDE_DISKS=""
 
-o_file="./tmp/cover.html"
-
-# setup the mail header.
+# Setup the mail header.  Replace the address with your own and modify subject line
+# if desired.
 (
-echo “To: receiver@domain.tld”
+echo “To: me@myaddress.com”
 echo “Subject: SMART Drive Results for all drives”
-echo “Content-Type: text/html”;
-echo “MIME-Version: 1.0″;
+echo “Content-Type: text/html”
+echo “MIME-Version: 1.0″
 echo ” ”
-echo “<html>”
-)>tmp/cover.html
+echo "<html>"
+) > $O_FILE
 
-#
-# For each device, look for bad sectors and get the disk temperature.
-#
-#c=0
-#for i in /dev/da0 /dev/da1 /dev/da2 /dev/da3 /dev/da4 /dev/da5 /dev/da6 /dev/da6 /dev/da7 /dev/da8 /dev/da9 /dev/ada0 /dev/ada01 /dev/ada02 /dev/ada03; do
+# Loop through the list of disks and retrieve various information
 
-#  results=$(smartctl -i -H -A -n standby -l error $i | grep -i 'test result’)
-#  badsectors=$(smartctl -i -H -A -n standby -l error $i | grep -i ‘Reallocated_Sector’ | awk ‘/Reallocated_Sector_Ct/ {print $10}’)
-##  temperature=$(smartctl -i -H -A -n standby -l error $i | grep -i ‘Temperature_Celsius’ | awk ‘/Temperature_Celsius/ {print $10}’)
-#  temperature=$(smartctl -i -H -A -n standby -l error $i | grep -i Temperature_Celsius | awk '/Temperature_Celsius/ {print $10}')
-#  ((c=c+1))
-# echo $c
-#  echo "Disk: $i"
-#  echo "$results"
+for i in $LIST_OF_DISKS 
+do
+  full_results=$(smartctl -a /dev/$i)
 
-#  if [[ $results == *”PASSED”* ]]
-#  then
-#    status[$c]=”Passed”
-#    color=”green”
-#  else
-#    status[$c]=”Failed”
-#    color=”red”
-#  fi
+  # Check to see if it is a USB device and set the device type to scsi
+  # NB: USB devices generally do not support SMART
 
-#  echo “$i status is ${status[$c]} with $badsectors bad sectors. Disk temperature is $temperature.”
-#  echo “<div style=’color: $color’> $i status is ${status[$c]} with $badsectors bad sectors. Disk temperature is $temperature.</div>” > tmp/cover.html
-#done
+  usb_dev=$(grep 'USB bridge' <<< $full_results)
+  if [[ -n "${usb_dev/[ ]*\n/}" ]]
+  then
+    full_results=" "
+    model="USB"
+    test_results="N/A"
+    bad_sectors="N/A"
+    temp="N/A"
 
-#echo “</html>” >> $o_file
+  else
+    model=$(grep 'Device Model' <<< $full_results |awk '/Device Model/ {print $3 $4}')
+    test_results=$(grep 'test result' <<< $full_results | awk '/test result/ {print $6}')
+    bad_sectors=$(grep 'Reallocated_Sector_Ct' <<< $full_results | awk '/Reallocated_Sector_Ct/ {print $10}')
+    temp=$(grep 'Temperature_Celsius' <<< $full_results | awk '/Temperature_Celsius/ {print $10}')
+  fi
+
+  echo "$i is a model $model with a status of $test_results and has $bad_sectors bad sectors.  Its temperature is $temp deg Celsius" >> $O_FILE    
+  echo "i = $i"
+done
+
+# Close the file and send it
+echo "</html>" >> $O_FILE
 
 #sendmail -t < ./tmp/cover.html
 
