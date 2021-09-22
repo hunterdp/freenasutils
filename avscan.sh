e0ca00ce6e0941e3f6bad271cf19b9c83c480a4c
David Hunter <dph@alumni.neu.edu>
2021-09-22 15:00:13 -0700

    Fixed formatting.

--- avscan.sh
+++ avscan.sh
@@ -1,61 +1,62 @@
 #!/bin/sh
 
-### Notes 															##
-## DPH - 20-Apr-2019														## 
-## - Copied from www.ixsystems.com/community/resources/howw-to-install-clamav-on-freenas-v11.66/				##
-## - Used to scan the shared windows home director used for backup.								##
-## DPH 22-Apr-2019
-## - Modified to store the log files in a tmp directory on the shared mount
-##																##
-## Shell scripts to update the ClamAV definations, then run a scan and prepare an email template 				##
-## This script is called from a master script running as a cron job on the FreeNAS server 					##
-## Master script is: run_clamav_scan.sh  											##
-##																##
-## Instructions: 														##
-## 1) To use this you need to create a Jail called "ClamAV" 									##
-## 2) Open a Shall to the jail and then run: "pkg update" 									##
-## 3) The run: "pkg install clamav" 												##
-## 4) You can then "exit" the Jail 												##
-## 5) Add the windows shares you wish to scan by using the Jail Add Storage feature 						##
-## 5a) Add the shares to same location you use in the variable: "scanlocation" 							##
-## 6) Setp a cronjob on the FreeNAS server to run a shell script on the FreeNAS server: "run_clamav_scan.sh" 			##
-## 7) The shell script "run_clamav_scan.sh" then connects to the Jail and runs this script. 					##
-## 8) Once finished, the "run_clamav_scan.sh" script emails a log to the email entered in the variable: "to_email" 		##
-##																##
-## https://www.clamav.net/ 													##
-## ClamAV is an open source (GPL) anti-virus engine used in a variety of situations including email scanning, web scanning, 	##
-## and end point security. It provides a number of utilities including a flexible and scalable multi-threaded daemon, a command ##
-## line scanner and an advanced tool for automatic database updates. 								##
-
-### Parameters ###
-## email address ##
+# DPH - 20-Apr-2019
+# - Copied from
+#   www.ixsystems.com/community/resources/howw-to-install-clamav-on-freenas-v11.66
+# - Used to scan the shared windows home director used for backup.
+#
+# DPH 22-Apr-2019
+# - Modified to store the log files in a tmp directory on the shared mount
+#
+# Shell scripts to update the ClamAV definations, then run a scan and prepare an
+# email template
+# This script is called from a master script running as a cron job on the FreeNAS ser
+# Master script is: run_clamav_scan.sh
+#
+# Instructions
+# 1) To use this you need to create a Jail called "Clam
+# 2) Open a Shall to the jail and then run: "pkg update"
+# 3) The run: "pkg install clamav"
+# 4) You can then "exit" the Jail
+# 5) Add the windows shares you wish to scan by using the Jail Add Storage feature
+# 5a) Add the shares to same location you use in the variable: "scanlocation
+# 6) Setp a cronjob on the FreeNAS server to run a shell script on the FreeNAS
+#    server: "run_clamav_scan.sh"
+# 7) The shell script "run_clamav_scan.sh" then connects to the Jail and runs this script.
+# 8) Once finished, the "run_clamav_scan.sh" script emails a log to the email entered in
+#    the variable: "to_email"
+#
+# https://www.clamav.net/
+# ClamAV is an open source (GPL) anti-virus engine used in a variety of situations including
+# email scanning, web scanning, and end point security. It provides a number of utilities
+# including a flexible and scalable multi-threaded daemon, a command
+# line scanner and an advanced tool for automatic database updates.
+#
+# Parameters
+#   email address
 to_email="dph@alumni.neu.edu"
 
-## Top directory of the files/directories you wish to scan, i.e. the "Jail Add Storage" locations ##
+# Top directory of the files/directories you wish to scan, i.e. the "Jail Add Storage" locations
 scanlocation="/mnt/home"
-### End ###
 
-### Update anti-virus definations ###
+# Update anti-virus definations
 freshclam -l /var/log/clamav/freshclam.log
-### End ###
 
-### Run the anti-virus scan ###
+# Run the anti-virus scan
 started=$(date "+ClamAV Scan on FreeNAS Share started at: %Y-%m-%d %H:%M:%S")
 clamscan -i -r -l /var/log/clamav/clamscan.log "${scanlocation}"
 finished=$(date "+ClamAV Scan finished at: %Y-%m-%d %H:%M:%S")
-### End ###
 
-### prepare the email ###
-## Set email headers ##
+# Set email headers
 (
     echo "To: ${to_email}"
     echo "Subject: ${started}"
     echo "MIME-Version: 1.0"
     echo "Content-Type: text/html"
     echo -e "\\r\\n"
-) > /tmp/clamavemail.tmp
+) >/tmp/clamavemail.tmp
 
-## Set email body ##
+# Set email body
 (
     echo "<pre style=\"font-size:14px\">"
     echo "${started}"
@@ -79,13 +80,8 @@ finished=$(date "+ClamAV Scan finished at: %Y-%m-%d %H:%M:%S")
     echo "--------------------------------------"
     tail -n +4 /var/log/clamav/clamscan.log | sed -e :a -e '$d;N;2,10ba' -e 'P;D'
     echo "</pre>"
-) >> /mnt/Sysadmin/scripts/tmp/clamavemail.tmp
+) >> /tmp/clamavemail.tmp
 
-### Tidy Up ###
-## Delete the freshclam log in preparation of a new log ##
+# Tidy Up
 rm /var/log/clamav/freshclam.log
-
-## Delete the clamscan log in preparation of a new log ##
 rm /var/log/clamav/clamscan.log
-### End ###
-
--- common_functions.sh
+++ common_functions.sh
@@ -4,18 +4,15 @@
 # sect title
 #	Display section title
 #
-sect()
-{
+sect() {
 	echo -e "${C_GREEN_S}${*}${C_GREEN_E}\n"
 }
 
-subsect()
-{
+subsect() {
 	echo -e "${C_YELLOW_S}${*}${C_YELLOW_E}"
 }
 
-subsubsect()
-{
+subsubsect() {
 	echo -e "${C_BOLD_S}${*}${C_BOLD_E}"
 }
 
@@ -23,8 +20,7 @@ subsubsect()
 # info message
 #	Display informational message to stdout.
 #
-info()
-{
+info() {
 	if checkyesno INFO_MSGS; then
 		echo -e "INFO: ${C_CYAN_S}${*}${C_CYAN_E}"
 	fi
@@ -34,12 +30,11 @@ info()
 # err exitval message
 #	Display message to stderr and exit with exitval.
 #
-err()
-{
+err() {
 	local _exitval=$1
 	shift
 
-	echo -e 1>&2 "`basename $0`: ERROR: ${C_RED_S}${*}${C_RED_E}"
+	echo -e "$(basename $0): ERROR: ${C_RED_S}${*}${C_RED_E}" 1>&2
 	exit $_exitval
 }
 
@@ -47,8 +42,7 @@ err()
 # warn message
 #	Display message to stderr.
 #
-warn()
-{
+warn() {
 	echo -e "WARNING: ${C_MAGENTA_S}${*}${C_MAGENTA_E}"
 }
 
@@ -56,14 +50,13 @@ warn()
 # debug message
 #	If debugging is enabled in sysinfo.conf output message to stderr.
 #
-debug()
-{
+debug() {
 	# XXX: we cannot use checkyesno() here as we would end up with
 	#      infinite loop.
 	case $DEBUG in
 	# "yes", "true", "on", or "1"
-	[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
-		echo -e 1>&2 "`basename $0`: DEBUG: ${C_BOLD_S}${*}${C_BOLD_E}"
+	[Yy][Ee][Ss] | [Tt][Rr][Uu][Ee] | [Oo][Nn] | 1)
+		echo -e "$(basename $0): DEBUG: ${C_BOLD_S}${*}${C_BOLD_E}" 1>&2
 		;;
 	esac
 }
@@ -73,8 +66,7 @@ debug()
 #	Retrun 0 if VERBOSE level is greater or equal to level,
 #	otherwise retrun 1.
 #
-is_verbose()
-{
+is_verbose() {
 	debug "VERBOSE is set to ${VERBOSE}."
 
 	if [ ${VERBOSE} -ge $1 ]; then
@@ -89,8 +81,7 @@ is_verbose()
 #	Check whether current user is root.
 #	Returns 0 if true, 1 otherwise.
 #
-is_root()
-{
+is_root() {
 	if [ "$(id -u)" != "0" ]; then
 		return 1
 	else
@@ -103,8 +94,7 @@ is_root()
 #	Check whether this system is running from within a jail.
 #	Retruns 1 if true, 0 otherwise.
 #
-is_jailed()
-{
+is_jailed() {
 	return $(sysctl -n security.jail.jailed)
 }
 
@@ -112,8 +102,7 @@ is_jailed()
 # check_privs path
 #	Checks path whether exists and is readble
 #
-check_privs()
-{
+check_privs() {
 	if [ -r $1 ]; then
 		return 0
 	elif [ -e $1 ] && ! is_root; then
@@ -131,8 +120,7 @@ check_privs()
 #	Reads the system configuration from system-wide
 #	configuration files.
 #
-getsysconf()
-{
+getsysconf() {
 	if [ -r /etc/defaults/rc.conf ]; then
 		debug "Sourcing /etc/defaults/rc.conf"
 		. /etc/defaults/rc.conf
@@ -152,20 +140,19 @@ getsysconf()
 #	Test $1 variable, and warn if not set to YES or NO.
 #	Return 0 if it is "yes", 1 otherwise.
 #
-checkyesno()
-{
+checkyesno() {
 	eval _value=\$${1}
 	debug "checkyesno: $1 is set to $_value."
 
 	case $_value in
 
-		# "yes", "true", "on", or "1"
-	[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
+	# "yes", "true", "on", or "1"
+	[Yy][Ee][Ss] | [Tt][Rr][Uu][Ee] | [Oo][Nn] | 1)
 		return 0
 		;;
 
 		# "no", "false", "off", or "0"
-	[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|[Oo][Ff][Ff]|0)
+	[Nn][Oo] | [Ff][Aa][Ll][Ss][Ee] | [Oo][Ff][Ff] | 0)
 		return 1
 		;;
 	*)
@@ -180,19 +167,17 @@ checkyesno()
 #	Print information of specified type from pciconf related
 #	to device.
 #
-getpciconf()
-{
+getpciconf() {
 	local _device _type i
 
 	_device=$1
 	_type=$2
 	i=5
 
-	if check_privs /dev/pci;  then
-		pciconf -lv | sed 's/ *= /=/' | while read line
-		do
+	if check_privs /dev/pci; then
+		pciconf -lv | sed 's/ *= /=/' | while read line; do
 			if echo $line | grep -q "^${1}"; then
-				i=$(( $i - 1 ))
+				i=$(($i - 1))
 			fi
 
 			# Each device produces 4 lines of data
@@ -200,7 +185,7 @@ getpciconf()
 				for t in $_type; do
 					echo $line | grep -w $t
 				done
-				i=$(( $i - 1 ))
+				i=$(($i - 1))
 			fi
 		done
 	fi
